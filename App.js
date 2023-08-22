5283b8815f05f0414d66cb972142c7ec5e41575c
Jukyung LEE <jk120412047@gmail.com>
2023-08-22 17:48:20 +0900

    HelpButton_Complete

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import * as React from 'react';
 import { Camera } from 'expo-camera';
 import * as Speech from 'expo-speech';
@@ -7,6 +7,18 @@ import { MaterialIcons } from '@expo/vector-icons';
 
 export default function App() {
   const [cameraPermission, setCameraPermission] = React.useState(null);
+  const [isOverlayVisible, setIsOverlayVisible] = React.useState(false);
+  const [speechIndex, setSpeechIndex] = React.useState(0);
+  const [isButtonsDisabled, setIsButtonsDisabled] = React.useState(false); // State to control button disabling
+
+  const overlayTexts = [
+    '도움말 버튼을 누르셨습니다. 다음 설명을 듣고싶으시면 화면을 터치해주세요.',
+    '어플의 첫 실행 화면은 카메라 화면입니다.',
+    '휴대폰을 사용자가 가려고하는 방향으로 비추면 어플이 장애물과의 거리를 인식하여 음성으로 알려줍니다.',
+    '화면 중앙 하단에는 카메라 촬영버튼이 있습니다. 이 버튼은 식품을 촬영하는 버튼으로 과자, 라면을 카메라로 촬영하면 상품을 인식하여 어떤 상품인지 알려줍니다.',
+    '화면 우측 상단에는 도움말 버튼이 있습니다. 어플의 사용법을 다시 듣고싶으시다면 우측 상단의 버튼을 눌러주세요.',
+    '어플 사용법 설명이 다 끝났습니다. 화면을 한번 더 터치하시면 기존 어플 화면으로 돌아갑니다.'
+  ];
 
   React.useEffect(() => {
     (async () => {
@@ -16,17 +28,27 @@ export default function App() {
   }, []);
 
   async function helpSound() {
+    setIsOverlayVisible(true);
+    setIsButtonsDisabled(true); // Disable buttons during speech playback
     try {
-      console.log('Playing TTS');
-      await Speech.speak('Help');
+      await Speech.speak(overlayTexts[speechIndex]);
+      setSpeechIndex(speechIndex + 1);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsButtonsDisabled(false); // Enable buttons after speech playback
     }
   }
 
+  React.useEffect(() => {
+    if (speechIndex >= overlayTexts.length) {
+      setIsOverlayVisible(true);
+    }
+  }, [speechIndex]);
+
   const handleCameraCapture = async () => {
-    if (!cameraPermission) {
-      console.log('Camera permission not granted.');
+    if (!cameraPermission || isButtonsDisabled) { // Prevent capture when buttons are disabled
+      console.log('Camera permission not granted or buttons are disabled.');
       return;
     }
 
@@ -38,6 +60,18 @@ export default function App() {
 
   const cameraRef = React.useRef(null);
 
+  const handleOverlayPress = () => {
+    if (isOverlayVisible) {
+      if (speechIndex < overlayTexts.length) {
+        helpSound();
+      } else {
+        setIsOverlayVisible(false);
+        setSpeechIndex(0);
+        setIsButtonsDisabled(false); // Enable buttons when overlay is dismissed
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar hidden={true} />
@@ -47,11 +81,19 @@ export default function App() {
         <Text>Camera permission denied. Please grant the permission.</Text>
       ) : (
         <View style={styles.cameraContainer}>
-          <TouchableOpacity style={styles.helpButton} onPress={helpSound}>
+          <TouchableOpacity style={styles.helpButton} onPress={helpSound} disabled={isButtonsDisabled}>
             <MaterialIcons name="help" size={24} color="white" />
           </TouchableOpacity>
+          {isOverlayVisible && (
+            <TouchableWithoutFeedback onPress={handleOverlayPress}>
+              <View style={styles.overlay} />
+            </TouchableWithoutFeedback>
+          )}
           <Camera style={styles.camera} ref={cameraRef} />
-          <TouchableOpacity style={styles.circularButton} onPress={handleCameraCapture}>
+          <TouchableOpacity
+            style={[styles.circularButton, isButtonsDisabled && styles.disabledButton]} 
+            onPress={handleCameraCapture}
+            disabled={isButtonsDisabled}>
             <MaterialIcons name="photo-camera" size={48} color="white" />
           </TouchableOpacity>
         </View>
@@ -90,5 +132,13 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 100,
     zIndex: 2,
+  },
+  disabledButton: {
+    backgroundColor: 'gray', // Style for disabled button
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    zIndex: 1,
   },
 });
