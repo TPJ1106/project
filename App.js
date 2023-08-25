0ec54bd82788024dd31b34c28f2e79333c327e8e
Jukyung LEE <jk120412047@gmail.com>
2023-08-25 15:16:15 +0900

    firstSound_Complete

--- App.js
+++ App.js
@@ -10,9 +10,11 @@ export default function App() {
   const [isOverlayVisible, setIsOverlayVisible] = React.useState(false);
   const [speechIndex, setSpeechIndex] = React.useState(0);
   const [isButtonsDisabled, setIsButtonsDisabled] = React.useState(false);
-  const [isFirstTime, setIsFirstTime] = React.useState(true);
+  const [hasPlayedFisrtTime, setHasPlayedFirstTime] = React.useState(false);
 
-  const overlayTexts = [
+  const firstText= '어플의 사용법을 알려드리겠습니다. 어플의 첫 실행 화면은 카메라 화면입니다. 휴대폰을 사용자가 가려고하는 방향으로 비추면 어플이 장애물과의 거리를 인식하여 음성으로 알려줍니다. 화면 중앙 하단에는 카메라 촬영버튼이 있습니다. 이 버튼은 식품을 촬영하는 버튼으로 과자, 라면을 카메라로 촬영하면 상품을 인식하여 어떤 상품인지 알려줍니다. 화면 우측 상단에는 도움말 버튼이 있습니다. 어플의 사용법을 듣고싶으시다면 우측 상단의 버튼을 눌러주세요. 어플 사용법 설명이 다 끝났습니다. 어플의 사용법을 듣고싶으시다면 다시 우측 상단의 도움말 버튼을 눌러주세요.';
+
+  const helpTexts = [
     '도움말 버튼을 누르셨습니다. 다음 설명을 듣고싶으시면 화면을 터치해주세요.',
     '어플의 첫 실행 화면은 카메라 화면입니다.',
     '휴대폰을 사용자가 가려고하는 방향으로 비추면 어플이 장애물과의 거리를 인식하여 음성으로 알려줍니다.',
@@ -25,29 +27,28 @@ export default function App() {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
       setCameraPermission(status === 'granted');
-
-      if (isFirstTime) {
-        setIsOverlayVisible(true);
-        setIsButtonsDisabled(true);
-        try {
-          await Speech.stop();
-          await Speech.speak("어플의 사용법을 알려드리겠습니다. 어플의 첫 실행 화면은 카메라 화면입니다. 휴대폰을 사용자가 가려고하는 방향으로 비추면 어플이 장애물과의 거리를 인식하여 음성으로 알려줍니다. 화면 중앙 하단에는 카메라 촬영버튼이 있습니다. 이 버튼은 식품을 촬영하는 버튼으로 과자, 라면을 카메라로 촬영하면 상품을 인식하여 어떤 상품인지 알려줍니다. 화면 우측 상단에는 도움말 버튼이 있습니다. 어플의 사용법을 듣고싶으시다면 우측 상단의 버튼을 눌러주세요. 어플 사용법 설명이 다 끝났습니다. 어플의 사용법을 듣고싶으시다면 다시 우측 상단의 도움말 버튼을 눌러주세요.");
-        } catch (error) {
-          console.error(error);
-        } finally {
-          setIsButtonsDisabled(false);
-        }
-        setIsFirstTime(false);
-      }
+      firstSound();
     })();
-  }, [isFirstTime]);
+  }, []);
+
+  async function firstSound() {
+    if (!hasPlayedFisrtTime){
+      try {
+        console.log('first TTS');
+        await Speech.speak(firstText);
+        setHasPlayedFirstTime(true);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 
   async function helpSound() {
     setIsOverlayVisible(true);
     setIsButtonsDisabled(true);
     try {
-      await Speech.stop();
-      await Speech.speak(overlayTexts[speechIndex]);
+      await Speech.stop(); // Stop any ongoing speech before starting a new one
+      await Speech.speak(helpTexts[speechIndex]);
       setSpeechIndex(speechIndex + 1);
     } catch (error) {
       console.error(error);
@@ -58,8 +59,8 @@ export default function App() {
 
   const handleOverlayPress = async () => {
     if (isOverlayVisible) {
-      if (speechIndex < overlayTexts.length) {
-        await Speech.stop();
+      if (speechIndex < helpTexts.length) {
+        await Speech.stop(); // Stop any ongoing speech before proceeding
         helpSound();
       } else {
         setIsOverlayVisible(false);
